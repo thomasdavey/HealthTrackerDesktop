5f137c855d30fcef28457a8beb33c9ff41cebf06
Amy Pryor <amy.a.p@hotmail.com>
2019-02-28 15:56:21 +0000

    Basic calculations needed for daily log

--- src/Model/Calculator.java
+++ src/Model/Calculator.java
@@ -59,24 +59,102 @@ public class Calculator {
         return metabolicRate;
     }
 
-    //method to calculate total energy expenditure
+    //ENERGY EXPENDITURE
+    //"Less than 2 hours per week" = 1.2
+    //"2-5 hours per week" = 1.375
+    //"5-10 hours per week" = 1.55
+    //"10-15 hours per week" = 1.725
+    //"More than 15 hours per week" = 1.9
 
+    public static int dailyCals(double metabolicRate, double energyExpenditure){
+        int dailyCals =(int) (metabolicRate*energyExpenditure);
 
-    //THIS METHOD IS NOT CORRECT YET
+        return dailyCals;
+    }
+
     //method that uses above two methods and desired weight loss/gain to calculate target calories
     //this must also calculate calorie deficit based on how extreme weight loss is
     //maybe give goal a numeric value that helps to calculate calories deficit.
-    public static int targetCalories(double metabolicRate, double weightLoss){
+    public static int targetCalories(double metabolicRate, double energyExpenditure, double weightLoss){
 
         int targetCalories = 0;
+        int dailyCals = dailyCals(metabolicRate, energyExpenditure);
 
-        //weightloss given a value -5 to 5. negative values are for weight loss, positive for gain
-        //-5 indicates a more extreme weight loss goal than -1
-        int calorieDeficit = (int) (weightLoss*100);
+        //weightloss given a value -3 to 3. negative values are for weight loss, positive for gain
+        //-3 indicates a more extreme weight loss goal than -1
+        int calorieDeficit = (int) (dailyCals*(weightLoss/10));
 
-        targetCalories = (int) (metabolicRate*1.15) - calorieDeficit;
+        targetCalories = (int) dailyCals - calorieDeficit;
 
         return targetCalories;
+    }
+
+
+    public static int targetBreakfast(int targetCalories){
+
+        int targetBreakfast = (int) (targetCalories*0.18);
+
+        return targetBreakfast;
+    }
+
+    public static int targetLunch(int targetCalories){
+
+        int targetLunch = (int) (targetCalories*0.3);
+
+        return targetLunch;
+    }
+
+
+    public static int targetDinner(int targetCalories){
+
+        int targetDinner = (int) (targetCalories*0.4);
+
+        return targetDinner;
+    }
+
+    public static int targetSnacks(int targetCalories){
+
+        int targetSnacks = (int) (targetCalories*0.12);
+
+        return targetSnacks;
+    }
+
+    public static int netCalories(int totalCalories, int exerciseCals){
+
+        int netCalories = totalCalories - exerciseCals;
+
+        return netCalories;
+    }
+
+    public static int calsUnderBudget(int targetCalories, int netCalories){
+
+        int calsUnderBudget = targetCalories - netCalories;
+
+        return calsUnderBudget;
+    }
+
+    public static int targetProtein(double weight){
+
+        //first convert weight to pounds
+        //protein intake should be 0.825 * weight in pounds
+        weight = (weight*2.20462);
+        int targetProtein = (int) (weight*0.825);
+
+        return targetProtein;
+    }
+
+    public static int targetFat(int dailyCals){
+
+        int targetFat = (int) ((dailyCals*0.25)/9);
+
+        return targetFat;
+    }
+
+    public static int targetCarbs(int dailyCals, int targetProtein, int targetFat){
+
+        int targetCarbs = (dailyCals - ((targetProtein*4) + (targetFat*9)))/4;
+
+        return targetCarbs;
     }
 
     //optional methods to calculate target fat/carbs/protein/sugar
@@ -85,8 +163,6 @@ public class Calculator {
 
     //method to predict when a user will meet their goal??
 
-    //methods calculating daily net calories, and calories under budget
-
     public static void main(String args[]){
 
         //testing the methods with my measurements
@@ -94,15 +170,30 @@ public class Calculator {
         System.out.println("ideal body weight: " + numFormat.format(idealBodyWeight(170, 'F')));
         double bmr = metabolicRate(74.8, 170, 19, 'F');
         System.out.println("bmr: " + numFormat.format(bmr));
-        System.out.println("target calories: " + targetCalories(bmr, 3));
+        int cals = targetCalories(bmr, 1.2, 3);
+        System.out.println("target calories: " + cals);
 
         //testing methods with toms measurements
-        System.out.println("bmi: " + numFormat.format(bmi(77.4, 182)));
+        /*System.out.println("bmi: " + numFormat.format(bmi(77.4, 182)));
         System.out.println("ideal body weight: " + numFormat.format(idealBodyWeight(182, 'M')));
         double bmr2 = metabolicRate(77.4, 182, 20, 'M');
         System.out.println("bmr: " + numFormat.format(bmr2));
         System.out.println("target calories: " + targetCalories(bmr2, 3));
+        */
 
+        System.out.println("Breakfast: " + targetBreakfast(cals));
+        System.out.println("Lunch: " + targetLunch(cals));
+        System.out.println("Dinner: " + targetDinner(cals));
+        System.out.println("Snacks: " + targetSnacks(cals));
+
+        int net = netCalories(1600, 400);
+        System.out.println("Net calories: " + net);
+        System.out.println("Calories under budget: " + calsUnderBudget(cals, net));
+
+        System.out.println("Target Protein: " + targetProtein(74.8) + "g");
+        System.out.println("Target Fat: " + targetFat(dailyCals(bmr, 1.5)) + "g");
+        System.out.println("Target Carbs: " + targetCarbs(dailyCals(bmr, 1.5),
+                targetProtein(74.8), targetFat(dailyCals(bmr, 1.5))) + "g");
     }
 
 }
