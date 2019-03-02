a6b0efcc88cab13df324e825be72db4495cecef0
Jamie Greasley <ata17yhu@uea.ac.uk>
2019-03-02 22:19:01 +0000

    - Added method LoadUser.getCaloriesByDate(String, Date)

--- src/DBClasses/DBAdd.java
+++ src/DBClasses/DBAdd.java
@@ -8,40 +8,17 @@ import java.sql.*;
 
 public final class DBAdd extends DBAccess{
 
-    private static Connection conn;
-    private static Statement stat;
-
     private DBAdd(){
 
-    }
-
-    @SuppressWarnings("Duplicates")
-    private static void getConnection(){
-        try {
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, UN, PW);
-            stat = conn.createStatement();
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void closeConnection(){
-        try {
-            conn.close();
-            stat.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public static void addUser(User u){
 
         getConnection();
         try {
-            stat.executeUpdate("INSERT INTO PROFILE VALUES ('"+u.getUserName()+"', '"+u.getPassword()+"', '"
+            st.executeUpdate("INSERT INTO PROFILE VALUES ('"+u.getUserName()+"', '"+u.getPassword()+"', '"
                     +u.getFirstName()+"', '"+u.getLastName()+"', '"+u.getEmail()+"', 1)");
-            stat.executeUpdate("INSERT INTO USER VALUES ('"+u.getUserName()+"', "+u.getHeight()+", "
+            st.executeUpdate("INSERT INTO USER VALUES ('"+u.getUserName()+"', "+u.getHeight()+", "
                     +u.getWeight()+", "+u.getBmi()+", "+u.getAge()+")");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -53,7 +30,7 @@ public final class DBAdd extends DBAccess{
     public static void addFood(Food f){
         getConnection();
         try {
-            stat.executeUpdate("INSERT INTO FOODS VALUES ('"+f.getName()+"', "+f.getKcals()+", "
+            st.executeUpdate("INSERT INTO FOODS VALUES ('"+f.getName()+"', "+f.getKcals()+", "
                     +f.getProtein()+", "+f.getCarbs()+", "+f.getFat()+", "+f.getSugar()+")");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -64,7 +41,7 @@ public final class DBAdd extends DBAccess{
     public static void addGoal(Goal g){
         getConnection();
         try {
-            stat.executeUpdate("INSERT INTO GOALS VALUES ('"+g.getUser()+"', '"
+            st.executeUpdate("INSERT INTO GOALS VALUES ('"+g.getUser()+"', '"
                     +g.getDescription()+"', "+g.isCompleted()+", '"+g.getCompletionDate()+"')");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -75,7 +52,7 @@ public final class DBAdd extends DBAccess{
     public static void addCalories(String u, Date d, int c){
         getConnection();
         try {
-            stat.executeUpdate("INSERT INTO CALORIECOUNTS VALUES ('"+u+"', '"+d+"', "+c+")");
+            st.executeUpdate("INSERT INTO CALORIECOUNTS VALUES ('"+u+"', '"+d+"', "+c+")");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -111,12 +88,12 @@ public final class DBAdd extends DBAccess{
 //        addGoal(goal);
 
         // Test adding calorie counts
-        addCalories("rodney", new Date(119, 1, 20), 1850);
-        addCalories("rodney", new Date(119, 1, 21), 2010);
-        addCalories("rodney", new Date(119, 1, 22), 1400);
-        addCalories("rodney", new Date(119, 1, 23), 2000);
-        addCalories("rodney", new Date(119, 1, 24), 1900);
-        addCalories("rodney", new Date(119, 1, 25), 1960);
-        addCalories("rodney", new Date(119, 1, 26), 2150);
+        addCalories("ladyinred", new Date(119, 1, 20), 1500);
+        addCalories("ladyinred", new Date(119, 1, 21), 910);
+        addCalories("ladyinred", new Date(119, 1, 22), 1400);
+        addCalories("ladyinred", new Date(119, 1, 23), 1200);
+        addCalories("ladyinred", new Date(119, 1, 24), 1330);
+        addCalories("ladyinred", new Date(119, 1, 25), 1060);
+        addCalories("ladyinred", new Date(119, 1, 26), 1333);
     }
 }
